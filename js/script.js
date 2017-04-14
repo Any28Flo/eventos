5398176c5b9454ebfd64a5f4ba1b46875f43225a
“Erika <foglzerika@gmail.com>
2017-04-13 21:49:56 -0500

    Agregando los los nuevos comentarios

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("click",cuentaClicks);
 var contadorCaracteres= document.getElementById("cometwit");
 contadorCaracteres.addEventListener("keydown",cuentaLetras);
 var btnComentario=document.getElementById("enviarComentario");
+btnComentario.addEventListener("click",twitNuevo);
 function cuentaClicks(){
   contador++;
   var contadorClicks= document.getElementById("contador");
@@ -12,28 +13,41 @@ function cuentaClicks(){
 function cuentaLetras(){
   contadorLetras++;
   var contadorCaracteres= document.getElementById("contadorLetras");
-  // console.log(contadorLetras);
   if(contadorLetras >=140)
   {
     btnComentario.disabled=true;
-    contadorCaracteres.style.color="red"; 
+    contadorCaracteres.style.color="red";
   }
-
   contadorCaracteres.innerHTML=contadorLetras;
 }
+function twitNuevo(){
+  var nodoPadre= document.getElementById("listaComentarios");
+  var nuevoTwit=document.createElement("div");
+  nodoPadre.appendChild(nuevoTwit);
 
-function creaComentario(){
-  var nodoPadre = document.getElementById("listaComentarios");
-  var creaComentario= document.createElement("div");
+  var seccionTwit= document.createElement("span");
+  nodoPadre.appendChild(seccionTwit);
+  var tuitStar= document.createElement("span");
+  nodoPadre.appendChild(tuitStar);
+  var comentario= document.getElementById("cometwit").value;
+  var duenoTwit= document.getElementById("deQuien").value;
+  seccionTwit.innerText=comentario;
+  tuitStar.innerText="De: "+duenoTwit;
 
-  var texto = document.getElementById("cometwit").value;
-  var deQuien = document.getElementById("deQuien").value;
 
-  document.createTextNode(deQuien);
-  var nodoTexto = document.createTextNode(texto);
-
-  nodoPadre.appendChild(creaComentario);
-  creaComentario.appendChild(nodoTexto);
-  creaComentario.appendChild(twitstar);
 }
-//contadorClic();
+// function creaComentario(){
+//   var nodoPadre = document.getElementById("listaComentarios");
+//   var creaComentario= document.createElement("div");
+//
+//   var texto = document.getElementById("cometwit").value;
+//   var deQuien = document.getElementById("deQuien").value;
+//
+//   document.createTextNode(deQuien);
+//   var nodoTexto = document.createTextNode(texto);
+//
+//   nodoPadre.appendChild(creaComentario);
+//   creaComentario.appendChild(nodoTexto);
+//   creaComentario.appendChild(twitstar);
+// }
+// //contadorClic();
